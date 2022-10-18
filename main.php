92333128bbc57cb657fff92f2c4b5389b02e167e
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-10-18 20:56:52 +0500

    Initial Commit

---
+++ main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page</title>
+
+    <!-- ========= bootstrap CSS file============ -->
+
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+
+    <!-- ============= Custom CSS File============
+ -->
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <a href="#" class="btn btn-primary">link</a>
+    <!-- =========== Custom Js File========== -->
+ <script src="main.js"></script>
+    <!-- ============= bootstrap Js Link============= -->
+<script src="./bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
